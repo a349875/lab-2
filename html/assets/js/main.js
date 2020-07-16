ca4d6d0da62634c2e251ccf61fd4ddf10b9b628e
HongYi <a349875@gmail.com>
2020-07-16 21:27:58 +0800

    html update

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Introspect by TEMPLATED
+	Broadcast by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
@@ -39,33 +39,108 @@
 				);
 			});
 
-		// Off-Canvas Navigation.
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
 
-			// Navigation Panel Toggle.
-				$('<a href="#navPanel" class="navPanelToggle"></a>')
-					.appendTo($body);
+		// Banner.
+			var $banner = $('#banner');
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						$('#nav').html() +
-						'<a href="#navPanel" class="close"></a>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left'
-					});
+			if ($banner.length > 0) {
 
-			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navPanel')
-						.css('transition', 'none');
+				// IE fix.
+					if (skel.vars.IEVersion < 12) {
+
+						$window.on('resize', function() {
+
+							var wh = $window.height() * 0.60,
+								bh = $banner.height();
+
+							$banner.css('height', 'auto');
+
+							window.setTimeout(function() {
+
+								if (bh < wh)
+									$banner.css('height', wh + 'px');
+
+							}, 0);
+
+						});
+
+						$window.on('load', function() {
+							$window.triggerHandler('resize');
+						});
+
+					}
+
+				// Video check.
+					var video = $banner.data('video');
+
+					if (video)
+						$window.on('load.banner', function() {
+
+							// Disable banner load event (so it doesn't fire again).
+								$window.off('load.banner');
+
+							// Append video if supported.
+								if (!skel.vars.mobile
+								&&	!skel.breakpoint('large').active
+								&&	skel.vars.IEVersion > 9)
+									$banner.append('<video autoplay loop><source src="' + video + '.mp4" type="video/mp4" /><source src="' + video + '.webm" type="video/webm" /></video>');
+
+						});
+
+				// More button.
+					$banner.find('.more')
+						.addClass('scrolly');
+
+			}
+
+		// Tabbed Boxes
+
+			$('.flex-tabs').each( function() {
+
+				var t 		= jQuery(this),
+					tab 	= t.find('.tab-list li a'),
+					tabs 	= t.find('.tab');
+
+				tab.click(function(e) {
+
+					var x = jQuery(this),
+						y = x.data('tab');
+
+					// Set Classes on Tabs
+						tab.removeClass('active');
+						x.addClass('active');
+
+					// Show/Hide Tab Content
+						tabs.removeClass('active');
+						t.find('.' + y).addClass('active');
+
+					e.preventDefault();
+
+				});
+
+			});
+
+		// Scrolly.
+			if ( $( ".scrolly" ).length ) {
+
+				var $height = $('#header').height();
+
+				$('.scrolly').scrolly({
+					offset: $height
+				});
+			}
 
 	});
 
